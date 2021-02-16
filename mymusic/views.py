7439c51389bde31639f83fa52b75c2879bf66a7b
khirod-datascience <66660324+khirod-datascience@users.noreply.github.com>
2021-02-16 11:19:55 +0530

    update

--- mymusic/views.py
+++ mymusic/views.py
@@ -10,29 +10,7 @@ def index(request):
     p=len(songs)
     products=list(musiclist.objects.all())
     products = random.sample(products, p)
-    songlist={}
-    #for song in products:
-        # songlist['Title']=song.Title
-        # songlist['Link']=song.Link
-        # songlist['Desc']=song.Desc
-        # songlist.append(song.Link)
-        # songlist.append(song.Title)
-        #print(song.Title)
-        #print(song)
-        #print(dir(song))
-    #random.shuffle(songlist)
-    # print(songlist)
-    # instance = vlc.Instance()
-    # player = instance.media_player_new()
-    # my_list = fl
-    # for song in fl:
-    #     if flag1 == "start":
-    #         player.set_mrl(song)
-    #         #player = vlc.MediaPlayer(song)
-    #         player.play()
-    #     if flag1 == "stop":
-    #         player.pause()
-    
+
     out1={'songs':songs,
             'songlist':songlist,
             'products':products}
