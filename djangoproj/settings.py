4067327157e72c8e9e74c7235ecf9169138c52b3
khirod-datascience <66660324+khirod-datascience@users.noreply.github.com>
2021-02-20 01:44:04 +0530

    Update settings.py

--- djangoproj/settings.py
+++ djangoproj/settings.py
@@ -123,16 +123,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 # STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
 # ]
 # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # # STATIC_ROOT='/static/'
 
 #----------Activate Django-Heroku.-------------
